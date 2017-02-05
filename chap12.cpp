b2b307c3dd2bc73e041b1a98ff47b08a218c0637
loneranger539 <saikoushik539@gmail.com>
2017-02-06 01:08:26 +0530

    chap 12 study

--- chap12.cpp
+++ chap12.cpp
@@ -11,6 +11,14 @@ using namespace std;
 void func(string &s3) {
 	s3 = "yy";
 }
+
+
+
+shared_ptr<string> func1(shared_ptr<string> a1) {
+	cout << "a1 count " << a1.use_count() << endl;
+	return a1;
+
+}
 int main() {
 	string a1("test");
 	string a2 = "test2";
@@ -37,11 +45,87 @@ int main() {
 	a2 = "55";
 	cout << "s1 is " << *s1 << endl;
 	cout << "s1 is " << s1.use_count() << endl;
-	shared_ptr<string> s1 = make_shared<string>(a2);
+//	shared_ptr<string> s1 = make_shared<string>(a2);
 
 	cout << "s1 is " << s1 << endl;
 
 	//dont use ordinary pointers when creating shared pointer 
 	//
+
+
+
+	shared_ptr<string> t1 = make_shared<string>("tt");
+	if (t1) {
+		cout << "no free" << endl;
+
+
+	}
+	else
+		cout << "free" << endl;
+
+	cout << "t1 count " << t1.use_count() << endl;
+	shared_ptr<string> t2,t3,t4;
+	t2 = t1;
+	cout << "t2 count " << t2.use_count() << endl;
+	cout << "t1 count " << t1.use_count() << endl;
+	t3 = t2;
+	cout << "t3 count " << t3.use_count() << endl;
+	cout << "t2 count " << t2.use_count() << endl;
+	cout << "t1 count " << t1.use_count() << endl;
+	t3 = t4;
+	t2 = t3;
+	//t4(t1);
+	cout << "t4 count " << t4.use_count() << endl;
+	cout << "t3 count " << t3.use_count() << endl;
+	cout << "t2 count " << t2.use_count() << endl;
+	cout << "t1 count " << t1.use_count() << endl;
+	shared_ptr<string> t5(t1);
+	cout << endl;
+	cout << "t5 count " << t5.use_count() << endl;
+	cout << "t4 count " << t4.use_count() << endl;
+	cout << "t3 count " << t3.use_count() << endl;
+	cout << "t2 count " << t2.use_count() << endl;
+	cout << "t1 count " << t1.use_count() << endl;
+	t4=func1(t5);
+	cout << endl;
+	cout << "t5 count " << t5.use_count() << endl;
+	cout << "t4 count " << t4.use_count() << endl;
+	cout << "t3 count " << t3.use_count() << endl;
+	cout << "t2 count " << t2.use_count() << endl;
+	cout << "t1 count " << t1.use_count() << endl;
+
+
+
+
+	//int *i1 = new int;
+	//cout << "i1 is " << i1 << endl;
+	//cout << "i1 is " << *i1 << endl;
+	//*i1 = 1;
+	//cout << "i1 is " << i1 << endl;
+	//cout << "i1 is " << *i1 << endl;
+
+	shared_ptr<string> t6 = t5;
+	cout << endl;
+	cout << "t5 count " << t5.use_count() << endl;
+	cout << "t4 count " << t4.use_count() << endl;
+	cout << "t3 count " << t3.use_count() << endl;
+	cout << "t2 count " << t2.use_count() << endl;
+	cout << "t1 count " << t1.use_count() << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	return 0;
 }
